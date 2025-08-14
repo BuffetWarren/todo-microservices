a5f957a871973d94c6f1d39b7bb4df10046eb2f2
Warren <fotiewarren50@gmail.com>
2025-08-14 23:05:33 +0100

    feat(configuration): prisma configuration

---
+++ person-service/prisma/migrations/20250814212444_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."Person" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Person_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Person_email_key" ON "public"."Person"("email");
